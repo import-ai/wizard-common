87e7aa54babfb8be2eaf94f8944f3b8ad2083943
Yichen Zhao <yichenzhao.yz@gmail.com>
2026-07-28 09:49:45 +0000

    feat(tasks): Add RSS item parser task

--- worker/entity.py
+++ worker/entity.py
@@ -15,6 +15,7 @@ class Base(BaseModel):
 class TaskFunction(StrEnum):
     COLLECT = "collect"
     COLLECT_URL = "collect_url"
+    PARSE_RSS_ITEM = "parse_rss_item"
     WEB_ANALYSIS = "web_analysis"
     UPSERT_INDEX = "upsert_index"
     DELETE_INDEX = "delete_index"
